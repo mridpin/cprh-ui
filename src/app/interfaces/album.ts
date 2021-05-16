336b4c30874f6ff9efadf6c2b9644cf991ca8cae
mridpin <mridao99@gmail.com>
2021-05-16 16:58:05 +0200

    Finish album interface and adatper

--- src/app/interfaces/album.ts
+++ src/app/interfaces/album.ts
@@ -1,7 +1,26 @@
+import { identifierModuleUrl } from "@angular/compiler";
+import { Injectable } from "@angular/core";
+import { Adapter } from "./adapter";
 
 export interface Album {
     id: string;
     name: string;
     artist: string;
     year: number;
+}
+
+// Injectable DTO object
+@Injectable({
+    providedIn: "root",
+})
+export class AlbumAdapter implements Adapter<Album> {
+    adapt(item: any): Album {
+        const album: Album = {
+            id: item.id,
+            name: item.name,
+            artist: item.artists[0].name,
+            year: new Date(item.release_date).getFullYear()
+        };
+        return album;
+    }
 }
