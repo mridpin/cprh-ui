49638b22b98755a6f513129ed81f280e826a3470
mridpin <mridao99@gmail.com>
2021-05-16 16:58:20 +0200

    Finish album interface and adatper

--- src/app/services/search.service.ts
+++ src/app/services/search.service.ts
@@ -1,28 +1,34 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { Album } from '../interfaces/album';
+import { Observable } from 'rxjs';
+import { map } from "rxjs/operators";
+
+import { environment } from 'src/environments/environment';
+import { Album, AlbumAdapter } from '../interfaces/album';
 
 @Injectable({
   providedIn: 'root'
 })
 export class SearchService {
 
-  constructor() { }
+  constructor(
+    private http: HttpClient,
+    private albumAdapter: AlbumAdapter
+  ) { }
 
-  search(): Album[] {
-    const res: Album[] = [
-      {
-        id: '1',
-        name: 'Name1',
-        artist: 'Artist1',
-        year: 2020,
-      },
-      {
-        id: '1',
-        name: 'Name1',
-        artist: 'Artist1',
-        year: 2020,
-      },
-    ];
-    return res;
+  /**
+   * Returns if:
+   * 200 -> Search results from the params from the server, mapped to a list of Album (can be empty)
+   * other code -> Error code and message
+   * @param searchParams 
+   */
+  search(searchParams: string): Observable<Album[]> {
+    return this.http.get<Album[]>(environment.url + '/search', {
+      params: {
+        search: searchParams
+      }
+    }).pipe(
+      map((res: any) => res.albums.items.map((item: any) => this.albumAdapter.adapt(item)))
+    );
   }
 }
