ea090ecfc8152a42318e68985c97398a33fe7031
mridpin <mridao99@gmail.com>
2021-07-10 19:35:33 +0200

    progress

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,15 +4,25 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { CopyrightComponent } from './copyright/copyright.component';
+import { SearchComponent } from './copyright/search/search.component';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+
+// Angular material
+import { MatSliderModule } from '@angular/material/slider';
+import { MaterialModule } from './modules/material.module';
 
 @NgModule({
   declarations: [
     AppComponent,
-    CopyrightComponent
+    CopyrightComponent,
+    SearchComponent
   ],
   imports: [
     BrowserModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    MatSliderModule,
+    ReactiveFormsModule,
+    MaterialModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
